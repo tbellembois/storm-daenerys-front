888498cf8bfd31d777cc9f1651860e6fe3b6ce6a
Thomas Bellembois <thomas.bellembois@uca.fr>
2023-12-12 10:24:51 +0100

    Cosmetics

--- src/ui/pages/main/panel_central.rs
+++ src/ui/pages/main/panel_central.rs
@@ -44,7 +44,7 @@ pub fn display_central_panel(app: &mut DaenerysApp, ctx: &Context, _frame: &mut 
             if app.application_just_loaded {
                 ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
                     ui.add_sized(
-                        [100., 100.],
+                        [40., 40.],
                         egui::Image::new(egui::include_image!(
                             "../../media/circle-question-regular.svg"
                         )),
@@ -89,30 +89,38 @@ pub fn display_central_panel(app: &mut DaenerysApp, ctx: &Context, _frame: &mut 
                     ui.label(format!("Check that this person is member of {} or {}-invite.", app.group_prefix.as_ref().unwrap(), app.group_prefix.as_ref().unwrap()));
                 });
 
+                ui.label("");
+
+                ui.separator();
                 
-                ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                    ui.label("Copyright: Université Clermont Auvergne");
+                ui.label("");
 
-                    ui.label("");
-
-                    ui.hyperlink("https://www.rust-lang.org/");
-                    ui.hyperlink("https://github.com/emilk/egui");
-                    ui.hyperlink("https://github.com/tokio-rs/axum");                 
-
-                    ui.label("");
-
+                ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
                     ui.add_sized(
                         [40., 40.],
                         egui::Image::new(egui::include_image!(
                             "../../media/rust.svg"
                         )),
                     );
+                });
 
-                    ui.label("");
-                    
+                ui.with_layout(egui::Layout::top_down(egui::Align::LEFT), |ui| {
                     ui.label(
                         egui::RichText::new("Credits").heading(),
                     );
+
+                    ui.label("");
+
+                    ui.hyperlink("https://www.rust-lang.org/");
+                    ui.hyperlink("https://github.com/emilk/egui");
+                    ui.hyperlink("https://github.com/tokio-rs/axum"); 
+
+                    ui.label("");
+
+                    ui.label(
+                        egui::RichText::new("Copyright").underline(),
+                    );
+                    ui.label("Université Clermont Auvergne");
                 });
             }
 
