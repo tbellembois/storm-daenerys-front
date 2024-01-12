9e86b7569efede8048f982cc14842a86b7ca101e
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-01-12 16:15:13 +0100

    Add compilation time debug.

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,17 @@ use ui::daenerys::DaenerysApp;
 fn main() -> Result<(), eframe::Error> {
     // Get compilation time information.
     let source_date_epoch = match env::var("SOURCE_DATE_EPOCH") {
-        Ok(val) => Utc.timestamp_opt(val.parse::<i64>().unwrap(), 0).unwrap(),
-        Err(_) => Utc::now(),
+        Ok(val) => {
+            dbg!("source_date_epoch: {}", &val);
+            Utc.timestamp_opt(val.parse::<i64>().unwrap(), 0).unwrap()
+        }
+        Err(e) => {
+            dbg!("can not get SOURCE_DATE_EPOCH: {}", e);
+            Utc::now()
+        }
     };
     let compilation_time = format!("{}", source_date_epoch.format("%d/%m/%Y %H:%M"));
+    dbg!("compilation_time: {}", &compilation_time);
 
     // Set window options.
     let options = eframe::NativeOptions {
